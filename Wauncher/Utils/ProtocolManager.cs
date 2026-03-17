ee0e44747906b6ea0dd41fffe258a5053d355da8
Nikolaus <github@koolych.ru>
2026-03-18 00:14:17 +1000

    Minor adjustments

--- Wauncher/Utils/ProtocolManager.cs
+++ Wauncher/Utils/ProtocolManager.cs
@@ -6,7 +6,7 @@ namespace Wauncher.Utils
     {
         public static void RegisterURIHandler()
         {
-            var appCurrentLocation = Path.Combine(new FileInfo(System.Environment.ProcessPath).Directory.FullName, "wauncher.exe");
+            var appCurrentLocation = Path.Combine(new FileInfo(Environment.ProcessPath).Directory.FullName, "wauncher.exe");
             EnsureKeyExists(Registry.CurrentUser, "Software/Classes/cc", "ClassicCounter");
             SetValue(Registry.CurrentUser, "Software/Classes/cc", "URL Protocol", string.Empty);
             EnsureKeyExists(Registry.CurrentUser, "Software/Classes/cc/DefaultIcon", $"{appCurrentLocation},1");
@@ -19,7 +19,7 @@ namespace Wauncher.Utils
             key.SetValue(valueName, value);
         }
 
-        private static RegistryKey EnsureKeyExists(RegistryKey rootKey, string keys, string defaultValue = null)
+        private static RegistryKey EnsureKeyExists(RegistryKey rootKey, string keys, string? defaultValue = null)
         {
             if (rootKey == null)
             {
