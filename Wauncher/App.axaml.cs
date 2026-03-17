e74aedf9e504e2ae63317e46147fb81b9f4f9857
Nikolaus <github@koolych.ru>
2026-03-18 01:02:42 +1000

    Version Control Oopsie

--- Wauncher/App.axaml.cs
+++ Wauncher/App.axaml.cs
@@ -22,7 +22,7 @@ namespace Wauncher
             if (!Argument.Exists("--skip-updates"))
             {
                 Services.LatestVersion = Task.Run(async () => await Wauncher.Utils.Version.GetLatestVersion()).GetAwaiter().GetResult();
-                if (Launcher.Utils.Version.Current != Services.LatestVersion) NewVersionAvailable = true;
+                if (Wauncher.Utils.Version.Current != Services.LatestVersion) NewVersionAvailable = true;
             }
         }
 
